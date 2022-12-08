28448d23e778e87e52806f953e773ba422be40ab
Marius <marius@example.com>
2022-12-08 09:41:01 +0200

    npm i router, 2 pages

--- readme.md
+++ readme.md
@@ -5,3 +5,5 @@
 1. https://formik.org/docs/tutorial
 2. https://formik.org/docs/tutorial#schema-validation-with-yup
 3. https://www.npmjs.com/package/yup
+
+
---
+++ src/pages/AddPostPage.jsx
@@ -0,0 +1,9 @@
+function AddPostPage(props) {
+  return (
+    <div>
+      <h1>AddPostPage</h1>
+      <p>this should be a form</p>
+    </div>
+  );
+}
+export default AddPostPage;
