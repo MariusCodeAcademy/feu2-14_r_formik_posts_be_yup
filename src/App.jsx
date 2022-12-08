76b220069332f6a692e2c7e78055beaf86f73fe9
Marius <marius@example.com>
2022-12-08 09:57:13 +0200

    header hav + logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
 import AddPostPage from './pages/AddPostPage';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className='App container'>
+      <Header />
       <Route path={'/add-post'}>
         <AddPostPage />
       </Route>
