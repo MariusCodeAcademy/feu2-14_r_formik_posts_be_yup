883be4da72ed9190a793c944508cf2229a589716
Marius <marius@example.com>
2022-12-08 09:44:10 +0200

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Route } from 'react-router-dom';
 import './App.css';
+import AddPostPage from './pages/AddPostPage';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>React</h1>
+    <div className='App container'>
+      <Route path={'/add-post'}>
+        <AddPostPage />
+      </Route>
+      <Route path={'/'} exact>
+        <HomePage />
+      </Route>
     </div>
   );
 }
