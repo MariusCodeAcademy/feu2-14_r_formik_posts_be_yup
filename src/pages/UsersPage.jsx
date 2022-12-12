8a38f1c4020388fbd0d94b6d477d97ff009a4f76
Marius <marius@example.com>
2022-12-12 15:38:24 +0200

    search works

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,16 +1,38 @@
 import SingleUser from '../components/users/SingleUser';
 import useFetch from '../hooks/useFetch';
+import { useState } from 'react';
 
 function UsersPage(props) {
   const [usersArr, setUsersArr] = useFetch();
-
+  const [searchTerm, setSearchTerm] = useState('');
   console.log('usersArr ===', usersArr);
+
+  // case sensitive
+  // const filteredUsers = usersArr.filter((uObj) =>
+  //   uObj.name.includes(searchTerm)
+  // );
+  // case insensitive
+  const filteredUsers = usersArr.filter((uObj) =>
+    uObj.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <div>
       <h1>Our users</h1>
+      <fieldset>
+        <legend>filter out</legend>
+        <input
+          onChange={(e) => setSearchTerm(e.target.value)}
+          value={searchTerm}
+          type='search'
+          placeholder='Search'
+        />
+        <button>Back end filter</button>
+      </fieldset>
+      <p>You have searched for: {searchTerm}</p>
+      <hr />
       <ul className='unlisted grid'>
-        {usersArr.map((uObj) => (
+        {filteredUsers.map((uObj) => (
           <SingleUser key={uObj.id} {...uObj} />
         ))}
       </ul>
