fd3db9df6452938721580c12dc79b0bc8703bb15
Marius <marius@example.com>
2022-12-12 15:59:20 +0200

    select started

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 function UsersPage(props) {
   const [usersArr, setUsersArr] = useFetch();
   const [searchTerm, setSearchTerm] = useState('');
+  const [selectValue, setSelectValue] = useState('paris');
   console.log('usersArr ===', usersArr);
 
   // case sensitive
@@ -26,6 +27,11 @@ function UsersPage(props) {
 
   const beOrFe = true ? filteredUsers : usersArr;
 
+  // use reduce, map filter to get all different towns
+  const allDifferentTownsArr = usersArr;
+
+  console.log('usersArr ===', JSON.stringify(usersArr));
+
   return (
     <div>
       <h1>Our users</h1>
@@ -41,6 +47,17 @@ function UsersPage(props) {
       </fieldset>
       <p>You have searched for: {searchTerm}</p>
       <hr />
+      <fieldset>
+        <legend>Select town</legend>
+        <select
+          onChange={(e) => setSelectValue(e.target.value)}
+          value={selectValue}
+        >
+          <option value='london'>London</option>
+          <option value='paris'>Paris</option>
+        </select>
+        <h3>Selected: {selectValue}</h3>
+      </fieldset>
       <ul className='unlisted grid'>
         {beOrFe.map((uObj) => (
           <SingleUser key={uObj.id} {...uObj} />
@@ -50,3 +67,33 @@ function UsersPage(props) {
   );
 }
 export default UsersPage;
+
+const allArr = [
+  {
+    name: 'James Bond 007',
+    town: 'Birmingam',
+    age: '29',
+    id: 1,
+    archived: false,
+  },
+  { name: 'Jill Krown', town: 'Dublin', age: '34', id: 2, archived: false },
+  { id: 3, name: 'Mike', age: 31, town: 'Capetown', archived: false },
+  { id: 4, name: 'Serbentautas', age: 18, town: 'Neveronys', archived: false },
+  {
+    name: 'Serbentautas Bordiuras',
+    age: 18,
+    town: 'Neveronys',
+    id: 5,
+    archived: true,
+  },
+  {
+    name: 'Serbentautas Bordiuras',
+    town: 'Serbija',
+    age: 35,
+    id: 6,
+    archived: true,
+  },
+  { name: 'Serbentautas', age: 18, town: 'Neveronys', id: 7, archived: false },
+  { name: 'Lenteja', town: 'Neveronys', age: '25', id: 8, archived: true },
+  { name: 'Grafas Kazkas', town: 'Kaunas', age: '35', archived: false, id: 9 },
+];
